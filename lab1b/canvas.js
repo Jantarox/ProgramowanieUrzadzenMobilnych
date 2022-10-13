f4c8bc05ceb86891ac08584f70f91a6878c340f5
Jantarox <228807@edu.p.lodz.pl>
2022-10-13 16:40:22 +0200

    Increase interval

--- lab1b/canvas.js
+++ lab1b/canvas.js
@@ -5,23 +5,28 @@ canvas.height = 600;
 
 var c = canvas.getContext("2d");
 
-setInterval(draw, 1000);
+setInterval(draw, 10);
+var i = 0;
 
 var color = "green";
 
-
 function draw(){
     c.beginPath();
-    c.arc(400, 300, 200, 0, 2 * Math.PI);
+    c.arc(100, 100, 50, 0, 2 * Math.PI);
     
-    
-    if(color === "green"){
-        color = "blue";
-    }else{
-        color = "green";
+    if(i==100){
+        if(color === "green"){
+            color = "blue";
+        }else{
+            color = "green";
+        }
+        i=0;
     }
-       
+    
+
     c.fillStyle = color;
     c.fill();
     c.stroke();
+
+    i++;
 }
