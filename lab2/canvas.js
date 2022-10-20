5fef28f768c0d74f59a0ac83111ba791cfa0e4a4
Jantarox <228807@edu.p.lodz.pl>
2022-10-20 16:26:33 +0200

    Add template files (index.html and canvas.js

---
+++ lab2/canvas.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = 800;
+canvas.height = 600;
+
+var c = canvas.getContext("2d");
+
+setInterval(draw, 10);
+var i = 0;
+di = 1;
+
+function draw(){
+    i+=di;
+}
