fb7a0349fbba30f70605f3dd4c68da7a09516bcd
Jantarox <228807@edu.p.lodz.pl>
2022-10-20 17:27:18 +0200

    Colision works?

--- lab2/canvas.js
+++ lab2/canvas.js
@@ -10,7 +10,7 @@ var i = 0;
 var di = 1;
 var g = 0.2;
 
-function Circle(x, y, dx, dy, r){
+function Circle(x, y, dx, dy, r) {
     this.x = x;
     this.y = y;
     this.dx = dx;
@@ -24,7 +24,7 @@ function Circle(x, y, dx, dy, r){
         c.stroke();
     }
 
-    this.update = function(){
+    this.update = function () {
         this.dy += g;
         //Colision with boundaries
         if (this.x > canvas.width - this.r || this.x < this.r) {
@@ -35,22 +35,23 @@ function Circle(x, y, dx, dy, r){
             this.dy *= 0.8;
         }
         rects.forEach(rect => {
-            
-            if (this.x + this.r > rect.x && this.x - r < rect.x + rect.w) {
-                this.dx *= -1;
-            }
-            if (this.y + this.r > rect.y && this.y - r < rect.y + rect.h) {
-                this.dx *= -1;
+
+            if (this.x + this.r > rect.x &&
+                this.x - this.r < rect.x + rect.width &&
+                this.y - this.r < rect.y + rect.height &&
+                this.y + this.r > rect.y) {
+                    this.dy *= -1;
+                    this.dy *= 0.8;
             }
         })
-        
+
         this.x += this.dx;
         this.y += this.dy;
         this.draw();
     }
 }
 
-function Rectangle(x, y, width, height){
+function Rectangle(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -65,28 +66,29 @@ function Rectangle(x, y, width, height){
 }
 
 var rects = [];
-function drawRects(){
+function drawRects() {
     rects.forEach(rect => rect.draw())
 }
 
-var circle = new Circle(20, 20, 1, 0, 20);
+var circle = new Circle(750, 20, 1, 0, 20);
 
 rects.push(new Rectangle(300, 500, 500, 100))
+rects.push(new Rectangle(700, 100, 100, 100))
+rects.push(new Rectangle(600, 200, 200, 100))
+rects.push(new Rectangle(500, 300, 300, 100))
+rects.push(new Rectangle(400, 400, 400, 100))
 
-function draw(){
-    i+=di;
+function draw() {
+    i += di;
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    
 
 
+
+
+
     
 
-    // rects.push(new Rectangle(700, 100, 100, 100))
-    // rects.push(new Rectangle(600, 200, 200, 100))
-    // rects.push(new Rectangle(500, 300, 300, 100))
-    // rects.push(new Rectangle(400, 400, 400, 100))
-    
     circle.update();
     drawRects();
 }
