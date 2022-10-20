8473cd0dd7ff1cc40c225b3fce2c4656f1b44b1b
Jantarox <228807@edu.p.lodz.pl>
2022-10-20 16:33:15 +0200

    Add rect class

--- lab2/canvas.js
+++ lab2/canvas.js
@@ -24,6 +24,13 @@ function Circle(x, y, dx, dy, r){
     }
 }
 
+function Rectangle(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+}
+
 function draw(){
     i+=di;
 }
