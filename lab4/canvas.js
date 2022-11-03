295d00a9c7c2585e7e43754a0eae000e80f26c55
Jantarox <228807@edu.p.lodz.pl>
2022-11-03 16:26:48 +0100

    Start preparing road

--- lab4/canvas.js
+++ lab4/canvas.js
@@ -34,21 +34,6 @@ function Circle(x, y, dx, dy, r) {
             this.dy *= -1;
             this.dy *= 0.8;
         }
-        rects.forEach(rect => {
-
-            if (this.x + this.r > rect.x &&
-                this.x - this.r < rect.x + rect.width &&
-                this.y - this.r < rect.y + rect.height &&
-                this.y + this.r > rect.y) {
-                if (this.y < rect.y || this.y > rect.y + rect.height) {
-                    this.dy *= -1;
-                    this.dy *= 0.8;
-                }
-                else if (this.x < rect.x || this.x > rect.x + rect.width) {
-                    this.dx *= -1;
-                }
-            }
-        })
 
         this.x += this.dx;
         this.y += this.dy;
@@ -56,24 +41,42 @@ function Circle(x, y, dx, dy, r) {
     }
 }
 
-function Rectangle(x, y, width, height) {
+function Rectangle(x, y, width, height, color) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.color = color;
 
     this.draw = function () {
         c.beginPath();
         c.rect(this.x, this.y, this.width, this.height);
-        c.strokeStyle = "black";
+        c.strokeStyle = this.color;
         c.stroke();
+    }
+
+    this.update = function () {
+        this.dy += g;
+        //Colision with boundaries
+        if (this.x > canvas.width - this.r || this.x < this.r) {
+            this.dx *= -1;
+        }
+        if (this.y > canvas.height - this.r || this.y < this.r) {
+            this.dy *= -1;
+            this.dy *= 0.8;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
     }
 }
 
-var rects = [];
+var road = new Rectangle(200, 0, 400, 600, "black");
 var circle;
 
 function draw() {
     i += di;
 
+    road.draw();
 }
