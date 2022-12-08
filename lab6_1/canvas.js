1c4a413b92e0fe981e4fa383fc3f69e9833f3ea5
Jantarox <228807@edu.p.lodz.pl>
2022-12-08 12:16:05 +0100

    Delete unnecessary code from lab2

--- lab6_1/canvas.js
+++ lab6_1/canvas.js
@@ -8,7 +8,6 @@ var c = canvas.getContext("2d");
 setInterval(draw, 10);
 var i = 0;
 var di = 1;
-var g = 0.2;
 
 function Circle(x, y, dx, dy, r) {
     this.x = x;
@@ -25,31 +24,6 @@ function Circle(x, y, dx, dy, r) {
     }
 
     this.update = function () {
-        this.dy += g;
-        //Colision with boundaries
-        if (this.x > canvas.width - this.r || this.x < this.r) {
-            this.dx *= -1;
-        }
-        if (this.y > canvas.height - this.r || this.y < this.r) {
-            this.dy *= -1;
-            this.dy *= 0.8;
-        }
-        rects.forEach(rect => {
-
-            if (this.x + this.r > rect.x &&
-                this.x - this.r < rect.x + rect.width &&
-                this.y - this.r < rect.y + rect.height &&
-                this.y + this.r > rect.y) {
-                if (this.y < rect.y || this.y > rect.y + rect.height) {
-                    this.dy *= -1;
-                    this.dy *= 0.8;
-                }
-                else if (this.x < rect.x || this.x > rect.x + rect.width) {
-                    this.dx *= -1;
-                }
-            }
-        })
-
         this.x += this.dx;
         this.y += this.dy;
         this.draw();
@@ -70,6 +44,10 @@ function Rectangle(x, y, width, height) {
     }
 }
 
+function GameBoard(){
+    
+}
+
 var rects = [];
 function drawRects() {
     rects.forEach(rect => rect.draw())
@@ -80,20 +58,6 @@ var circle;
 function draw() {
     i += di;
     c.clearRect(0, 0, innerWidth, innerHeight);
-    if (i == 100) {
-        rects.push(new Rectangle(300, 500, 500, 100))
-    } else if (i == 200) {
-        rects.push(new Rectangle(400, 400, 400, 100))
-    } else if (i == 300) {
-        rects.push(new Rectangle(500, 300, 300, 100))
-    } else if (i == 400) {
-        rects.push(new Rectangle(600, 200, 200, 100))
-    } else if (i == 500) {
-        rects.push(new Rectangle(700, 100, 100, 100))
-    } else if (i == 600) {
-        circle = new Circle(750, 20, 1, 0, 20);
-    } else if (i > 600) {
-        circle.update();
-    }
+
     drawRects();
 }
